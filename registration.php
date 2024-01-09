0082d5c2459af4d346652ead1bf34fbdc3929395
aniketkale-1563 <99478932+aniketkale-1563@users.noreply.github.com>
2024-01-09 11:00:58 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,16 +1,16 @@
 <?php     //start php tag
-//include connect.php page for database connection
+//include connection.php page for database connection
 Include('connect.php')
 //if submit is not blanked i.e. it is clicked.
 If(isset($_REQUEST['submit'])!='')
 {
-If($_REQUEST['full_name']=='' || $_REQUEST['current_academic_year']=='' || $_REQUEST['department']=='')
+If($_REQUEST['full_name']=='' || $_REQUEST['current_academic_year']=='' || $_REQUEST['department']=='' || $_REQUEST['id_card']=='' || $_REQUEST['fee_receipt']=='' || $_REQUEST['purpose']=='')
 {
 Echo "please fill the empty field.";
 }
 Else
 {
-$sql="insert into student(full_name,current_academic_year,department) values('".$_REQUEST['full_name']."', '".$_REQUEST['current_academic_year']."', '".$_REQUEST['department']."')";
+$sql="insert into student(full_name,current_academic_year,department) values('".$_REQUEST['full_name']."', '".$_REQUEST['current_academic_year']."', '".$_REQUEST['department']."','".$_REQUEST['id_card']."','".$_REQUEST['fee_receipt']."','".$_REQUEST['purpose']."')";
 $res=mysql_query($sql);
 If($res)
 {
