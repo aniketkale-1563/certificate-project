01ee241b83916af288288a84666059952cd00015
aniketkale-1563 <99478932+aniketkale-1563@users.noreply.github.com>
2024-01-11 14:43:29 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,28 +1,33 @@
 
-<?php     //start php tag
-//include connection.php page for database connection
-Include('connect.php')
-//if submit is not blanked i.e. it is clicked.
-If(isset($_REQUEST['submit'])!='')
-{
-If($_REQUEST['full_name']=='' || $_REQUEST['current_academic_year']=='' || $_REQUEST['department']=='' || $_REQUEST['id_card']=='' || $_REQUEST['fee_receipt']=='' || $_REQUEST['purpose']=='')
-{
-Echo "please fill the empty field.";
-}
-Else
-{
-$sql="insert into student(full_name,current_academic_year,department,id_card,fee_receipt,purpose) values('".$_REQUEST['full_name']."', '".$_REQUEST['current_academic_year']."', '".$_REQUEST['department']."','".$_REQUEST['id_card']."','".$_REQUEST['fee_receipt']."','".$_REQUEST['purpose']."')";
-$res=mysql_query($sql);
-If($res)
-{
-Echo "Record successfully inserted";
-}
-Else
-{
-Echo "There is some problem in inserting record";
+
+<?php
+// Connect to MySQL (change these parameters based on your setup)
+$conn = new mysqli("localhost", "root", "", "bonafide_details");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-}
+// Process form data.
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+    $fullname = $_POST["fullname"];
+    $academicyear = $_POST["academicyear"];
+    $department = $_POST["department"];
+    $id_card = $_POST["id_card"];
+    $fee_receipt = $_POST["fee_receipt"];
+    $purpose = $_POST["purpose"];
+
+    // Insert student data into the database.
+    $sql = "INSERT INTO student (full_name, current_academic_year, department,id_card,fee_receipt,purpose) VALUES ('$fullname','$academicyear','$department','$id_card','$fee_receipt','$purpose')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Student record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 
+$conn->close();
 ?>
